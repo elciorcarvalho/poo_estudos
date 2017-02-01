2ac9868176b5dc578b6bc2ca4485880c2030c5f7
Elcior Santana <elciorfscarvalho@gmail.com>
2017-02-01 08:47:44 -0300

    Update aglutinacao.php
    Alteração em aglutinação.php

--- aglutinacao.php
+++ aglutinacao.php
@@ -8,19 +8,16 @@ require_once 'classes/Cesta.php';
 $c1 = new Cesta();
 
 $p1 = new Produto('Maçã', 5, 3.8);
-/*$p2 = new Produto('Pelucia', 1, 55);
-$p3 = new Produto('Caixa de Bombom', 1, 19);*/
-$c1->addItens($p1);
+$p2 = new Produto('Pelucia', 1, 55);
+$p3 = new Produto('Caixa de Bombom', 1, 19);
 
-/*for($i = 1; $i < 4; $i++){
+for($i = 1; $i < 4; $i++){
 	$var = 'p' . $i;
 	$c1->addItens($$var);
-}*/
+}
 
-/*print_r($c1);*/
-
-foreach($item as $c1->getItens()->getdescricao()) {
-	echo 'Item: ' . $item . "<br>\n";
+foreach($c1->getItens() as $item) {
+	echo 'Item: ' . $item->getDescricao() . "<br>\n";
 }
 
 ?>
