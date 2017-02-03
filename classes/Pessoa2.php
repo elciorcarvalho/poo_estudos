ccec41206eb69b03e643f879b885a5c8a025f3d1
elciorcarvalho <elciorfscarvalho@gmail.com>
2017-02-03 13:52:30 -0200

    Estudo de array em constantes para POO

--- classes/Pessoa2.php
+++ classes/Pessoa2.php
@@ -1,14 +1,24 @@
 <?php
-class Huamano
+class Pessoa2
 {
 	protected $nome;
 	protected $sexo;
+	const SEXO = array("F" => "Feminino", "M" => "Masculino");
 
 	public function __construct($n, $s)
 	{
 		$this->nome = $n;
-
+		$this->sexo = $s;
 		
 	}
+
+	public function getNome()
+	{
+		return $this->nome;
+	}
+
+	public function getSexo()
+	{
+		return self::SEXO[$this->sexo];
+	}
 }
-array("F" => "Feminino", "M" => "Masculino");
--- constante.php
+++ constante.php
@@ -1,8 +1,17 @@
 <?php
 require_once 'classes/Conta.php';
 require_once 'classes/ContaPoupanca.php';
+require_once 'classes/Pessoa2.php';
 
 $cp = new ContaPoupanca('123456789', '1234');
 echo "Essa é a constante " . ContaPoupanca::GERENTE . "<br>\n";
+echo "Este é o gerente da {$cp->getInfo()}: {$cp->getGerente()}<br>\n";
 
-echo "Este é o gerente da {$cp->getInfo()}: {$cp->getGerente()}";
+echo "<hr>";
+
+$p1 = new Pessoa2('Maria dos Anjos', 'F');
+$p2 = new Pessoa2('José Silva', 'M');
+echo "Nome: {$p1->getNome()}<br>\n";
+echo "Sexo: {$p1->getSexo()}<br>\n";
+echo "Nome: {$p2->getNome()}<br>\n";
+echo "Sexo: {$p2->getSexo()}<br>\n";
